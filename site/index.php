def5d07259a88a2687b74cf68f3f790d27e290ff
JoaoGabrielAnjosLeal <joaogabrieel79@gmail.com>
2022-12-21 19:20:35 -0300

    Acerto de background do player

--- site/index.php
+++ site/index.php
@@ -1,7 +1,7 @@
 <?php include('include/header.php'); ?>
 <div class="player">
-    <img class="player-leftSide" src="<?php $_SERVER['HTTP_HOST'];?>/canal_start/site/assets/img/bg-player.left">
-    <img class="player-rightSide float-end" src="<?php $_SERVER['HTTP_HOST'];?>/canal_start/site/assets/img/bg-player.right">
+    <img class="player-leftSide" src="<?php $_SERVER['HTTP_HOST'];?>/canal_start/site/assets/img/bg-player.left.webp" title="Fundo lado esquerdo">
+<img class="player-rightSide float-end" src="<?php $_SERVER['HTTP_HOST'];?>/canal_start/site/assets/img/bg-player.right.webp" title="Fundo lado direito">
     <div class="player-container">
         <div id="MediaPlayerMiniPlayer">Loading...</div>
     </div>
