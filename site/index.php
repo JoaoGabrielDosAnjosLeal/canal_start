db0488c1bea5b79d07176817c49281730a477a89
JoaoGabrielAnjosLeal <joaogabrieel79@gmail.com>
2022-12-22 21:05:58 -0300

    speednews

--- site/index.php
+++ site/index.php
@@ -1,9 +1,58 @@
 <?php include('include/header.php'); ?>
+
 <div class="player">
     <img class="player-leftSide" src="<?php $_SERVER['HTTP_HOST'];?>/canal_start/site/assets/img/bg-player.left.webp" title="Fundo lado esquerdo">
 <img class="player-rightSide float-end" src="<?php $_SERVER['HTTP_HOST'];?>/canal_start/site/assets/img/bg-player.right.webp" title="Fundo lado direito">
     <div class="player-container">
         <div id="MediaPlayerMiniPlayer">Loading...</div>
+    </div>
+</div>
+
+<div class="speedNews">
+    <nav class="speedNews-navbar navbar bg-light">
+        <div class="speedNews-container container-fluid mx-auto">
+            <h6 class="speedNews-title navbar-brand w-100 text-center text-uppercase">Speed News</h6>
+        </div>
+    </nav>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xll-12">
+                <section class="speedNews-container d-flex flex-wrap gap-3 mt-4 mb-4">
+                    <article class="speedNews-article">
+                        <img class="speedNews-thumb" src="https://criticalhits.com.br/wp-content/uploads/2022/04/My-Dress-Up-Darling-anime-manga.jpg" alt="Thumb da speed news">
+                        <h6 class="speedNews-author"><i class="bi bi-person-circle"></i>&nbsp;Takashi</h6>
+                        <h6 class="speedNews-date"><i class="bi bi-calendar-date-fill"></i>&nbsp;00/00/0000</h6>
+                        <p class="speedNews-text p-1">
+                            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable Eng
+                        </p>
+                    </article>
+                    <article class="speedNews-article">
+                        <img class="speedNews-thumb" src="https://www.intoxianime.com/wp-content/uploads/2021/07/4ff8304c8113335ee40a9448239dc11d1626614065_main.jpg" alt="Thumb da speed news">
+                        <h6 class="speedNews-author"><i class="bi bi-person-circle"></i>&nbsp;Takashi</h6>
+                        <h6 class="speedNews-date"><i class="bi bi-calendar-date-fill"></i>&nbsp;00/00/0000</h6>
+                        <p class="speedNews-text p-1">
+                            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable Eng
+                        </p>
+                    </article>
+                    <article class="speedNews-article">
+                        <img class="speedNews-thumb" src="https://1.bp.blogspot.com/-YhI4TJ0TKiI/YA_9c8x1yWI/AAAAAAAALfA/i8eI51-BECsATF1QTzfMU9E_G8otGIyyACNcBGAsYHQ/s1920/20210115202721_1.jpg" alt="Thumb da speed news">
+                        <h6 class="speedNews-author"><i class="bi bi-person-circle"></i>&nbsp;Takashi</h6>
+                        <h6 class="speedNews-date"><i class="bi bi-calendar-date-fill"></i>&nbsp;00/00/0000</h6>
+                        <p class="speedNews-text p-1">
+                            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable Eng
+                        </p>
+                    </article>
+                    <article class="speedNews-article">
+                        <img class="speedNews-thumb" src="https://webstatic.hoyoverse.com/upload/contentweb/2022/12/09/ffcf148dfaabe1f3e7b0b84016966f43_1811971358991344843.jpg" alt="Thumb da speed news">
+                        <h6 class="speedNews-author"><i class="bi bi-person-circle"></i>&nbsp;Takashi</h6>
+                        <h6 class="speedNews-date"><i class="bi bi-calendar-date-fill"></i>&nbsp;00/00/0000</h6>
+                        <p class="speedNews-text p-1">
+                            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable Eng
+                        </p>
+                    </article>
+                </section>
+            </div>
+        </div>
     </div>
 </div>
 
